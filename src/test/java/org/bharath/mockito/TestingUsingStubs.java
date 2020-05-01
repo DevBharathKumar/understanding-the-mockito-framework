1359de91901632f59c3afe15cbd3f76a32b9a155
Bharath Kumar <bharathk494@gmail.com>
2020-05-01 16:27:46 +0530

    Disadvantage of using the stubs

--- src/test/java/org/bharath/mockito/TestingUsingStubs.java
+++ src/test/java/org/bharath/mockito/TestingUsingStubs.java
@@ -13,6 +13,13 @@ class TestingUsingStubs
 		NumberComparitionServiceImpl numberComparitionServiceImpl = new NumberComparitionServiceImpl(new ThreeTestStub());
 		assertEquals(109, numberComparitionServiceImpl.getLargestNumber());
 	}
+	
+	@Test
+	void testingOneNumber() 
+	{
+		NumberComparitionServiceImpl numberComparitionServiceImpl = new NumberComparitionServiceImpl(new OneTestStub());
+		assertEquals(1, numberComparitionServiceImpl.getLargestNumber());
+	}
 
 }
 
@@ -22,6 +29,17 @@ class ThreeTestStub implements DataService
 	@Override
 	public int[] getNumbers() {
 		return new int [] {1,45,109};
+	}
+	
+}
+
+//Disadvantage of the stub is that each time we need to create a new class to pass the data 
+class OneTestStub implements DataService
+{
+
+	@Override
+	public int[] getNumbers() {
+		return new int [] {1};
 	}
 	
 }
